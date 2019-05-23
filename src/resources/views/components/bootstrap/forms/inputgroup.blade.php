b2aed800cf2631dba137ba22b76fc235d2ab5965
Txomin Medrano Martorell <tmedrano@tarragona.cat>
2019-05-23 12:11:17 +0200

    bug inputgroup

--- src/resources/views/components/bootstrap/forms/inputgroup.blade.php
+++ src/resources/views/components/bootstrap/forms/inputgroup.blade.php
@@ -1,9 +1,11 @@
 <div 
 	@isset($id) id="{{$id}}" @endif
 
-	class="input-group {{ $class or '' }} @isset($size) input-group-{{ $size }}  @endif "
-	{!! html_attributes(isset($attributes)?$attributes:false) !!}
+	class="input-group {{ isset($class)?$class:''  }} @isset($size) input-group-{{ $size }}  @endif "
+		
+		{!! html_attributes(isset($attributes)?$attributes:false) !!}
    		{!! html_attributes(isset($data)?$data:false,'data') !!}
+		
 >
 	{{ $slot }}
 </div>
