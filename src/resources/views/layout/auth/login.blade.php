a0deeb10ea994ea5891d2682c737caae6660896e
Txomin Medrano Martorell <tmedrano@tarragona.cat>
2019-07-18 12:12:32 +0200

    typography montserrat and logo

--- src/resources/views/layout/auth/login.blade.php
+++ src/resources/views/layout/auth/login.blade.php
@@ -9,30 +9,44 @@
 	<div class="col-lg-4 col-md-6 col-sm-8">
 		<div class="card mt-5" >
 			<div class="card-body">				
-				<h4 class="card-title">{{ config('app.name')}}</h4>
-				<h6 class="card-subtitle mb-4">@lang('tgn::auth.login-form')</h6>
+				
+				
+				<div class="row">
+					<div class="col-sm-4 col-6">
+						<img src="{{ asset('vendor/ajtarragona/img/logo-tgn-negre.png')}}" title="tgn" class="img-fluid"/>
+						<h2 class="card-title" style="text-transform: lowercase">{{ config('app.name')}}</h2>
+						<h6 class="card-subtitle mb-4">@lang('tgn::auth.login-form')</h6>
+				
+					</div>
+					
+					<div class="col-sm-8 ">
+							@form(['method'=>'POST','action'=>route('tgn.dologin')])
+			                      
+							@input(['container'=>true, 'icon'=>'user', 'name'=>'username', 'label'=>__("tgn::auth.username"), 'value'=>old('name')])
+							@input(['container'=>true, 'icon'=>'lock', 'type'=>'password', 'name'=>'password', 'label'=>__("tgn::auth.password")])
+							{{-- @checkbox(['name'=>'remember', 'label'=>'Remember Me']) --}}
+												
+						 
+							<div class="btn-group mt-2 text-right" >
+								@button(['type'=>'submit'])
+									@lang('tgn::auth.login') @icon('sign-in-alt') 
+								@endbutton
+		
+								{{-- @include('ajtarragona-web-components::auth._messages') --}}
+		
+							   {{--  <a class="btn btn-link" href="{{ route('password.request') }}">
+												Forgot Your Password?
+											</a> --}}
+							</div>
+		
+						 @endform
+					</div>
+					
+				</div>
+				
 						
 
-			    @form(['method'=>'POST','action'=>route('tgn.dologin')])
-			                      
-			        @input(['container'=>true, 'icon'=>'user', 'name'=>'username', 'label'=>__("tgn::auth.username"), 'value'=>old('name')])
-			        @input(['container'=>true, 'icon'=>'lock', 'type'=>'password', 'name'=>'password', 'label'=>__("tgn::auth.password")])
-			        {{-- @checkbox(['name'=>'remember', 'label'=>'Remember Me']) --}}
-			                            
-			     
-			        <div class="btn-group mt-4">
-			            @button(['type'=>'submit'])
-			                @lang('tgn::auth.login') @icon('sign-in-alt') 
-			            @endbutton
-
-			            {{-- @include('ajtarragona-web-components::auth._messages') --}}
-
-			           {{--  <a class="btn btn-link" href="{{ route('password.request') }}">
-			                            Forgot Your Password?
-			                        </a> --}}
-			        </div>
-
-			     @endform
+			    
 
 
 			</div>
