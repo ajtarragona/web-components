406c8b1042ecf409bad057d0101099fa3100bd76
Txomin Medrano Martorell <tmedrano@tarragona.cat>
2019-12-04 15:40:04 +0100

    isjson

--- src/Helpers/json.php
+++ src/Helpers/json.php
@@ -3,7 +3,9 @@
 if (! function_exists('isJson')) {
 	function isJson($string) {
 	 	try{
-	 		json_decode($string);
+			$ret=json_decode($string);
+			if(!is_array($ret) && !is_object($ret)) return false; //es un tipo simple
+			 
 			return (json_last_error() == JSON_ERROR_NONE);
 		}catch(Exception $e){
 			return false;
