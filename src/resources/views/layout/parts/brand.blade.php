bd197cecc944228892a1a73093366b93e7f7c0c9
Txomin Medrano Martorell <tmedrano@tarragona.cat>
2020-05-19 09:25:27 +0200

    home link

--- src/resources/views/layout/parts/brand.blade.php
+++ src/resources/views/layout/parts/brand.blade.php
@@ -1,3 +1,12 @@
 <div class="brand">
-	<h5 class="text-white">@icon(config('webcomponents.icon','globe'),['size'=>'lg']) {{ config('app.name') }} </h5>
+	
+	<h5 class="text-white">
+		@if(\Route::has('home') )
+			<a href="{{ route('home') }}" class="text-white">
+		@endif
+		@icon(config('webcomponents.icon','globe'),['size'=>'lg']) {{ config('app.name') }} 
+		@if(\Route::has('home') )
+			</a>
+		@endif
+	</h5>
 </div>
