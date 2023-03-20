968b37c709205b4039980b2e67bc6fdd5b66bd88
Txomin <tmedrano@tarragona.cat>
2023-03-20 14:20:31 +0100

    class to code

--- src/resources/views/components/bootstrap/code.blade.php
+++ src/resources/views/components/bootstrap/code.blade.php
@@ -2,7 +2,8 @@
 	@isset($id) id="{{$id}}" @endif
 	class="prettyprint theme-{{ $theme ?? 'dark'}} 
 		{!! (isset($linenums) && $linenums)?'linenums':'' !!}
-		{!! (isset($lang))?'lang-'.$lang:'' !!}
+		{!! (isset($lang))?'lang-'.$lang:'' !!} 
+		{{ $class ?? ''}}
 	"
 
 	data-copy="{!! isset($copy)?($copy?"true":"false"):"true" !!}"
