e52361422fd3f40763b1136cc0e6eb799c05e1ba
Txomin <tmedrano@tarragona.cat>
2024-02-22 11:31:50 +0100

    bug sidebar

--- src/resources/views/layout/parts/sidebar.blade.php
+++ src/resources/views/layout/parts/sidebar.blade.php
@@ -6,7 +6,7 @@
             <a class="sidebar-toggle ml-auto " href="#" data-session-setting="sidebar" data-session-toggle="true" data-session-value="{{ session('sidebar') }}" >@icon('bars')</a>
         </header>
 
-        <div id="main-menu" class="flex-grow-1 d-flex">
+        <div id="main-menu" class="flex-grow-1 ">
 	        @includeIf('layout.menu')
 	        
 			@yield('menu')
