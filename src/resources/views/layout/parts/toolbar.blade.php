d72b4aabc01e1bfffc5cea73d4649cade5968d55
Txomin Medrano <tmedrano@tarragona.cat>
2026-05-29 09:47:23 +0200

    toolbar flex

--- src/resources/views/layout/parts/toolbar.blade.php
+++ src/resources/views/layout/parts/toolbar.blade.php
@@ -1,9 +1,9 @@
 <div id="maintoolbar" >
 	<div class="toolbar-toggle">@icon("ellipsis-v")</div>
 	<div class="toolbar-content ">
-		<div class="container-fluid">
-			<div class="row align-items-center">
-				<div class="col-md-6 the-breadcrumb">
+		{{-- <div class="container-fluid"> --}}
+			<div class="d-block d-md-flex justify-content-between align-items-center px-3">
+				<div class="the-breadcrumb flex-grow-1 flex-shrink-1" style="min-width: 0;">
 					{{-- <h3 class=" pb-0 mb-0">@yield('title')</h3> --}}
 					
 					 @hasSection('breadcrumb')
@@ -13,13 +13,13 @@
 	              	@endif
 
 				</div>
-				<div class="col-md-6 text-md-right py-3 the-actions">
+				<div class="flex-1 text-md-right py-3 the-actions flex-shrink-0">
 					 @hasSection('actions')
 							@yield('actions')
 					@endif
 				</div>
 			</div>
-		</div>
+		{{-- </div> --}}
 		
 	</div>
 </div>
